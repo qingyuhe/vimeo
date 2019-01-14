5e947b229fb34567e7ae66c1cad0d56d6a53043d
qingyuhe <hecynthia@gmail.com>
2019-01-14 15:37:07 -0500

    Updates

---
+++ precache-manifest.ad50aa2ce614e39b71546fefd50c6cb3.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "23ac19ffc9e0261c1ef3",
+    "url": "/vimeo/static/js/runtime~main.23ac19ff.js"
+  },
+  {
+    "revision": "2e989c14476d709471a2",
+    "url": "/vimeo/static/js/main.2e989c14.chunk.js"
+  },
+  {
+    "revision": "0f5d1015c5ba3c54f6d7",
+    "url": "/vimeo/static/js/1.0f5d1015.chunk.js"
+  },
+  {
+    "revision": "2e989c14476d709471a2",
+    "url": "/vimeo/static/css/main.5b862033.chunk.css"
+  },
+  {
+    "revision": "c586cd58a459716b99a9e159be0cfdfb",
+    "url": "/vimeo/index.html"
+  }
+];
